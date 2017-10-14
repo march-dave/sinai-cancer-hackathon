2db40bf2a5abfd2c82845c732392a47391d42882
march-dave <ysangy@gmail.com>
2017-10-14 18:14:30 -0400

    pag add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  YesClick() {
+    console.log('Yes');
+    // alert('Yes');    
+    // window.location = '/search/'+this.state.query+'/some-action';
+    window.location = 'http://google.com';
+  }
+
+  NoClick(e) {
+    console.log('No', e);
+
+    alert('No');
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,15 +28,16 @@ class App extends Component {
 
         <p className="App-intro">
           
-        <table>
-        <tbody><tr> 
-            <td>Did you know?</td>
-          </tr>
-          <tr>
-            <td>Yes</td>
-            <td>No</td>
-          </tr>
-        </tbody></table>
+          <div style={{border: '0px solid black'}}> 
+            <div style={{ margin: 80, padding: 80, border: '1px solid black' }}>
+              {/* <div style={{float: 'left', width: '50%'}}>Cell 1</div> */}
+              <div>Do you Smoke?</div>
+            </div>
+            <div style={{ margin: 80, padding: 80, border: '0px solid black' }}>
+              <div style={{float: 'left', width: '50%'}}><button onClick={this.YesClick.bind(this)}>Yes</button></div>
+              <div style={{float: 'left', width: '50%'}}><button onClick={this.NoClick.bind(this)}>No</button></div>
+            </div>
+          </div>
 
         </p> 
 
