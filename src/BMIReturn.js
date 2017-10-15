cc4aa94678f68bc01d1def989625f3263a7a887f
march-dave <ysangy@gmail.com>
2017-10-15 10:31:35 -0400

    up to date

--- src/BMIReturn.js
+++ src/BMIReturn.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { Button, FormControl, ControlLabel, FormGroup, HelpBlock, Jumbotron } from 'react-bootstrap';
+import './App.css';
 
 class BMIReturn extends Component {
     render() {
         return (
-            <div>
-                BMIReturn
+            <div style={{margin: 80, padding: 80}}>
+            
+                <Jumbotron>
+                    {/* <h2>Underweight BMI less than 18.5</h2> */}
+                    <h2>Your Underweight BMI is 28.3</h2>
+
+                    <div style={{backgroundColor: 'green'}}><h2>healthy 18.5 - 24.9</h2></div>
+                    <div style={{backgroundColor: 'yellow'}}><h2>overweight > 25</h2></div>
+                    <div style={{backgroundColor: 'red'}}><h2>overweight BMI > 30</h2></div>
+
+                </Jumbotron>
 
                 <p>
                 {/* under weight BMI < 18.5 */}
